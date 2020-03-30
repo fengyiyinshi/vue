e02e60c19f71d8245f788fcf9549ab4e6d3fa2d7
fengyiyinshi <58417756+fengyiyinshi@users.noreply.github.com>
2020-03-30 10:41:20 +0800

    Add files via upload

---
+++ src/layouts/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- <loading/> -->
+    <Header/>
+    <router-view></router-view>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+  
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import loading from '../components/loading.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,Footer,loading
+  }
+}
+</script>
+
+<style>
+
+</style>
